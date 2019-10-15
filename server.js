a36ef85ce1f040e05610524b5243724339790cab
david-mcgrouther <dbmcgrouther@gmail.com>
2019-10-15 14:43:06 -0700

    getting situated

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Database
-const db = require('./models');
-
-// ---------------------------------- TEMP DATA
-//ANCHOR put fakedata here
+//ANCHOR need db part?
+// const db = require('./models');
 
 // --------------------------------- MIDDLEWARE
 // Serve the Public Directory
@@ -26,27 +24,107 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //-------------------------------- ROUTES
+
+
+// ---------------- HTML routes -----------------
 // Home Route
 // app.get(path, callback function)
 //ANCHOR this is essentially the home page 
 app.get('/', (req, res) => {
-  res.sendFile('views/index.html', {
-    root: __dirname,
+    res.sendFile('views/index.html', {
+      root: __dirname,
+    });
   });
-});
 
-// API ROUTES
-// GET Users Index
-//ANCHOR this to be the "business page", showing users
-app.get('/api/v1/users', (req, res) => {
-  db.User.find({}, (error, allUsers) => {
-    if (error) return console.log(error);
-    res.json({
-      status: 200,
-      count: allUsers.length,
-      data: allUsers,
-      dateRequested: new Date().toLocaleString()
+// GET About Us Route
+app.get('/about-us', (req, res) => {
+    res.sendFile('views/about.html', {
+      root: __dirname,
     });
   });
   
-});
+  
+  // GET Contacts Index Route
+  app.get('/contacts', (req, res) => {
+    res.sendFile('/views/contacts.html', {
+      root: __dirname,
+    });
+  });
+  
+  
+  // GET New Contact Route
+  app.get('/contacts/new', (req, res) => {
+    res.sendFile('/views/new.html', {
+      root: __dirname,
+    });
+  });
+  
+  
+  // GET Contacts Route
+  app.get('/contact-us', (req, res) => {
+    res.sendFile('/views/contact-us.html', {
+      root: __dirname,
+    });
+  });
+  
+  
+  // GET Contacts Show Route
+  app.get('/contacts/:id', (req, res) => {
+    res.sendFile('/views/show.html', {
+      root: __dirname,
+    });
+  });
+  
+  
+  // -------------------------- API ROUTES ---------------------- //
+  // THESE ROUTES SERVE DATA TO OUR TEMPLATES THROUGH API CALLS (AJAX)
+  
+  // GET Contacts Index
+  app.get('/api/v1/contacts', (req, res) => {
+    db.Contact.find({}, (err, allContacts) => {
+      if (err) return console.log(err);
+  
+      res.json({
+        status: 200,
+        count: allContacts.length,
+        data: allContacts,
+        dataRequested: new Date().toLocaleString(),
+      });
+    });
+  });
+  
+  
+  // GET Contacts Show
+  app.get('/api/v1/contacts/:id', (req, res) => {
+    db.Contact.findById(req.params.id, (err, contact) => {
+      if (err) return console.log(err);
+  
+      res.json({
+        status: 200,
+        count: 1,
+        data: contact,
+        dataRequested: new Date().toLocaleString(),
+      });
+    });
+  });
+  
+  
+  // POST Contacts Create
+  app.post('/api/v1/contacts', (req, res) => {
+    // console.log(req.body);
+    // res.sendStatus(201);
+    db.Contact.create(req.body, (err, newContact) => {
+      if (err) return console.log(err);
+  
+      res.json({
+        status: 201,
+        count: 1,
+        data: newContact,
+        dataRequested: new Date().toLocaleString(),
+      });
+    });
+  });
+  
+  
+  // -------------------------- START SERVER ---------------------- //
+  app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
